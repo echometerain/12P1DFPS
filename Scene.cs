97e46e0372fce170eb38ba3b35227bf27c2f84a2
rain1598 <guacamolek@yandex.com>
2020-10-11 12:25:47 -0400

    learning c#

--- Scene.cs
+++ Scene.cs
@@ -1,21 +1,13 @@
 using Godot;
 using System;
 
-public class Scene : Node2D
-{
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
+public class Scene : Node2D{
+    public sbyte WorldY = 20;
+    public sbyte WorldX = 20;
+    public override void _Ready(){
+        sbyte[][] map = new sbyte[WorldY][WorldX];
     }
+    public override void _Process(float delta){
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    }
 }
