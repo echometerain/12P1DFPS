262b13c3cafdd427b935449ad969eb59e1b3e488
rain1598 <guacamolek@yandex.com>
2020-10-12 17:32:10 -0400

    n

--- Driver.cs
+++ Driver.cs
@@ -3,19 +3,27 @@ using System;
 
 public class head : Node
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public class driver {
+		public Vector2 unit;
+		public int lum;
+		public void put(int y, int x, int lum){
+			unit = new Vector2(y, x);
+			this.lum = lum;
+		}
+	}
+	const int WorldY = 21;
+	const int WorldX = 21;
+	Vector2 Pos = new Vector2(WorldX/2, WorldY/2); //player position
+	byte Life = 255;
+	byte Ammo = 255;
+	byte Weapon = 255;
+	//as viewed from the top right quarter (+, +)
+	//refering to rtype and horizontal & vertical
+	driver[] fhor = {};
+	driver[] fver = {};
+	driver[] shor = {};
+	driver[] sver = {};
+	driver[] ahor = {};
+	driver[] aver = {};
+	driver[] c = {};
 }
