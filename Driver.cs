ae7d94b55b05a1597dd39b93800c224f75a32c24
rain1598 <guacamolek@yandex.com>
2020-10-13 17:37:39 -0400

    objected

--- Driver.cs
+++ Driver.cs
@@ -1,8 +1,7 @@
 using Godot;
 using System;
 
-public class Driver : Node
-{
+public class Driver : Node{
     public class driver {
 		public Vector2 unit;
 		public int dis; //distance
@@ -105,86 +104,54 @@ public class Driver : Node
 		switch(angle){
 			case 0:
 				render(fver, true, true, false);
+				return;
+			case 6:
+				render(fver, true, true, false);
+				return;
+			case 12:
+				render(fver, true, true, false);
+				return;
+			case 18:
+				render(fver, true, true, false);
+				return;
+		}
+		bool xplus = false;
+		bool yplus = false;
+		switch(angle/4){
+			case 0:
+				xplus = true; yplus = true;
 				break;
 			case 1:
-				render(sver, true, true, false);
+				xplus = true; yplus = false;
 				break;
 			case 2:
-				render(aver, true, true, false);
+				xplus = false; yplus = false;
 				break;
 			case 3:
-				render(c, true, true, false);
+				xplus = false; yplus = true;
+				break;
+		}
+		switch(angle/6){
+			case 1:
+				render(sver, xplus, yplus, true);
+				break;
+			case 2:
+				render(aver, xplus, yplus, true);
+				break;
+			case 3:
+				render(c, xplus, yplus, true);
 				break;
 			case 4:
-				render(aver, true, true, true);
+				render(aver, xplus, yplus, false);
 				break;
 			case 5:
-				render(sver, true, true, true);
-				break;
-
-			case 6:
-				render(fver, true, true, true);
-				break;
-			case 7:
-				render(sver, true, false, false);
-				break;
-			case 8:
-				render(aver, true, false, false);
-				break;
-			case 9:
-				render(c, true, false, false);
-				break;
-			case 10:
-				render(aver, true, false, true);
-				break;
-			case 11:
-				render(sver, true, false, true);
-				break;
-
-			case 12:
-				render(fver, true, false, false);
-				break;
-			case 13:
-				render(sver, false, false, false);
-				break;
-			case 14:
-				render(aver, false, false, false);
-				break;
-			case 15:
-				render(c, false, false, false);
-				break;
-			case 16:
-				render(aver, false, false, true);
-				break;
-			case 17:
-				render(sver, false, false, true);
-				break;
-
-			case 18:
-				render(fver, true, false, true);
-				break;
-			case 19:
-				render(sver, false, true, false);
-				break;
-			case 20:
-				render(aver, false, true, false);
-				break;
-			case 21:
-				render(c, false, true, false);
-				break;
-			case 22:
-				render(aver, false, true, true);
-				break;
-			case 23:
-				render(sver, false, true, true);
-				break;
-				
-			default:
-				GD.PrintErr("rendered default "+ angle);
+				render(sver, xplus, yplus, false);
 				break;
 		}
 	}
 	public void render(driver[] drivers, bool Xplus, bool Yplus, bool sxy){
-		
+		foreach(driver e in drivers){
+			
+		}
 	}
 }
